74a260585fe65891fe254181e6c74ace9f2c6313
Saeed Haddadi <saeid.special@gmail.com>
2015-09-11 16:22:37 +0200

    Finding location without user interaction!!

--- app/src/main/java/de/berlin/special/concertmap/InitiateFragment.java
+++ app/src/main/java/de/berlin/special/concertmap/InitiateFragment.java
@@ -1,15 +1,10 @@
 package de.berlin.special.concertmap;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -27,30 +22,55 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
 import de.berlin.special.concertmap.service.DataFetchService;
 
 /**
  * Created by Saeed on 18-Nov-14.
  */
-public class InitiateFragment extends Fragment {
+public class InitiateFragment extends Fragment implements ConnectionCallbacks, OnConnectionFailedListener{
 
     private static final String LOG_TAG = InitiateFragment.class.getSimpleName();
-    private static final String noAddressFound = "No address found.";
-    private static final String locationNotFound = "Location not found.";
-    private static final String enterTheCity = "Enter the city.";
-    private LocationManager locMgr;
+    private static final String enterTheCity = "Location is not available, Enter the city.";
+
+    private static final int locationAvailable = 100;
+    private static final int locationNotAvailable = 101;
+
+    private String lastKnownLocation;
     private LinearLayout locationFoundLayout;
-    private LinearLayout locationNotFoundLayout;
     private LinearLayout addressNotFoundLayout;
     private ProgressBar progressIndicator;
     private TextView locationView;
     private TextView commentView;
-    private ImageButton locationRefreshBtn;
     private ImageButton searchCityBtn;
     private Button continueBtn;
 
+    // Client to request last known location
+    private GoogleApiClient mGoogleApiClient;
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+    // Status of connection to google location service
+    private int connStat;
+    // Location to be retrieved
+    private Location mLastLocation;
 
-    public InitiateFragment() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GoogleApiClient instance
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
     @Override
@@ -58,37 +78,30 @@ public class InitiateFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_start, container, false);
 
-        locMgr = (LocationManager)
-                getActivity().getSystemService(Context.LOCATION_SERVICE);
-
         progressIndicator = (ProgressBar) rootView.findViewById(R.id.address_progress);
         locationFoundLayout = (LinearLayout) rootView.findViewById(R.id.locationFoundLayout);
-        locationNotFoundLayout = (LinearLayout) rootView.findViewById(R.id.locationNotFoundLayout);
         addressNotFoundLayout = (LinearLayout) rootView.findViewById(R.id.addressNotFoundLayout);
-        locationNotFoundLayout = (LinearLayout) rootView.findViewById(R.id.locationNotFoundLayout);
         locationView = (TextView) rootView.findViewById(R.id.location_view);
         commentView = (TextView) rootView.findViewById(R.id.comment_view);
-        locationRefreshBtn = (ImageButton) rootView.findViewById(R.id.location_refresh_button);
         searchCityBtn = (ImageButton) rootView.findViewById(R.id.validate_city_button);
         continueBtn = (Button) rootView.findViewById(R.id.continue_button);
+
         return rootView;
     }
 
     @Override
     public void onStart() {
         super.onStart();
-         updateConcertInfo();
+        if (!mResolvingError) {
+            mGoogleApiClient.connect();
+        }
     }
+
+
 
     @Override
     public void onResume() {
         super.onResume();
-        getAddress();
-        /* Temporary:
-        addressNotFoundLayout.setVisibility(View.INVISIBLE);
-        locationNotFoundLayout.setVisibility(View.INVISIBLE);
-        locationFoundLayout.setVisibility(View.VISIBLE);
-        locationView.setText("Berlin");*/
 
         continueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,174 +113,94 @@ public class InitiateFragment extends Fragment {
         });
     }
 
-    // Fetching data from remote server.. concerts info from last.fm
-    private void updateConcertInfo(){
-        Intent intent = new Intent(getActivity(), DataFetchService.class);
-        intent.putExtra(DataFetchService.LOCATION_EXTRA, "Berlin");
-        getActivity().startService(intent);
-    }
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        getLastKnownLocation(mLastLocation);
 
-    public void getAddress() {
-        // Ensure that a Geocoder services is available
-        if (Build.VERSION.SDK_INT >=
-                Build.VERSION_CODES.GINGERBREAD
-                &&
-                Geocoder.isPresent()) {
-            // Show the activity indicator
-            progressIndicator.setVisibility(View.VISIBLE);
-            /*
-             * Reverse geocoding is long-running and synchronous.
-             * Run it on a background thread.
-             * Pass the current location to the background task.
-             * When the task finishes,
-             * onPostExecute() displays the address.
-             */
+        if (connStat == locationNotAvailable) {
 
-            if (!locMgr.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
-
-                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
-                dialog.setMessage("Concert Map needs to access your location. Please turn on location access.");
-                dialog.setPositiveButton("Setting", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                        startActivityForResult(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS), 100);
-                    }
-                });
-                dialog.setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                        // TODO Auto-generated method stub
-                    }
-                });
-                dialog.show();
-            }
-            else {
-                Location loc = locMgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                (new GetAddressTask(getActivity())).execute(loc);
-            }
-        }
-    }
-
-    private class GetAddressTask extends AsyncTask<Location, Void, String> {
-        Context mContext;
-
-        public GetAddressTask(Context context) {
-            super();
-            mContext = context;
-        }
-
-        /**
-         * Get a Geocoder instance, get the latitude and longitude
-         * look up the address, and return it
-         *
-         * @params params One or more Location objects
-         * @return A string containing the address of the current
-         * location, or an empty string if no address can be found,
-         * or an error message
-         */
-        @Override
-        protected String doInBackground(Location... params) {
-
-            Geocoder geocoder =
-                    new Geocoder(mContext, Locale.getDefault());
-            // Get the current location from the input parameter list
-
-            Location loc = params[0];
-
-            if (loc != null) {
-
-                // Create a list to contain the result address
-                List<Address> addresses = null;
-                try {
-
-                 /*Return 1 address.*/
-
-                    addresses = geocoder.getFromLocation(loc.getLatitude(),
-                            loc.getLongitude(), 1);
-                } catch (IOException e1) {
-                    Log.e(LOG_TAG, "IO Exception in getFromLocation()");
-                    e1.printStackTrace();
-                    return ("IO Exception trying to get address");
-                } catch (IllegalArgumentException e2) {
-                    // Error message to post in the log
-                    String errorString = "Illegal arguments " +
-                            Double.toString(loc.getLatitude()) +
-                            " , " +
-                            Double.toString(loc.getLongitude()) +
-                            " passed to address service";
-                    Log.e(LOG_TAG, errorString);
-                    e2.printStackTrace();
-                    return errorString;
+            locationFoundLayout.setVisibility(View.INVISIBLE);
+            addressNotFoundLayout.setVisibility(View.VISIBLE);
+            commentView.setText(enterTheCity);
+            searchCityBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(),
+                            "Button is clicked", Toast.LENGTH_LONG).show();
                 }
-                // If the reverse geocode returned an address
-                if (addresses != null && addresses.size() > 0) {
-                    // Get the first address
-                    Address address = addresses.get(0);
+            });
+        } else if (connStat == locationAvailable) {
 
-                 /** Format the first line of address (if available),
-                 city, and country name.*/
-
-                    String addressText = String.format(
-                            "%s, %s",
-                            // Locality is usually a city
-                            address.getLocality(),
-                            // The country of the address
-                            address.getCountryName());
-                    // Return the City + Country
-                    return addressText;
-
-                } else {
-                    // return noAddressFound;
-                    return noAddressFound;
-                }
-            } else {
-                return locationNotFound;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String address) {
-            // Set activity indicator visibility to "gone"
+            addressNotFoundLayout.setVisibility(View.INVISIBLE);
+            locationFoundLayout.setVisibility(View.VISIBLE);
             progressIndicator.setVisibility(View.GONE);
-            // Display the results of the lookup.
-            if (address.equals(noAddressFound)){
-                locationFoundLayout.setVisibility(View.INVISIBLE);
-                locationNotFoundLayout.setVisibility(View.INVISIBLE);
-                addressNotFoundLayout.setVisibility(View.VISIBLE);
-                commentView.setText(enterTheCity);
-                searchCityBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(getActivity(),
-                                "Button is clicked", Toast.LENGTH_LONG).show();
-                    }
-                });
-
-            } else
-            if (address.equals(locationNotFound)){
-                addressNotFoundLayout.setVisibility(View.INVISIBLE);
-                locationFoundLayout.setVisibility(View.INVISIBLE);
-                locationNotFoundLayout.setVisibility(View.VISIBLE);
-                commentView.setText(locationNotFound);
-                locationRefreshBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        commentView.setText(null);
-                        Location loc = locMgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        (new GetAddressTask(getActivity())).execute(loc);
-                    }
-                });
-
-            } else {
-                addressNotFoundLayout.setVisibility(View.INVISIBLE);
-                locationNotFoundLayout.setVisibility(View.INVISIBLE);
-                locationFoundLayout.setVisibility(View.VISIBLE);
-                locationView.setText(address);
-            }
-
+            locationView.setText(lastKnownLocation);
+            // Fetching data from last.fm based on retrieved location
+            Intent intent = new Intent(getActivity(), DataFetchService.class);
+            intent.putExtra(DataFetchService.LOCATION_EXTRA, "Berlin");
+            getActivity().startService(intent);
         }
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+        return;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(getActivity(), REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        } else {
+            // Show dialog using GoogleApiAvailability.getErrorDialog()
+            Log.d(LOG_TAG, String.valueOf(result.getErrorCode()));
+            mResolvingError = true;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    public void getLastKnownLocation(Location loc) {
+
+        Geocoder gcd = new Geocoder(getContext(), Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = gcd.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Exception in getFromLocation()");
+            e.printStackTrace();
+            connStat = locationNotAvailable;
+        }
+
+        if (addresses != null && addresses.size() > 0) {
+
+            Address address = addresses.get(0);
+            String addressText = String.format(
+                    "%s, %s",
+                    // Locality is usually a city
+                    address.getLocality(),
+                    // The country of the address
+                    address.getCountryName());
+            // Return the City + Country
+            lastKnownLocation = addressText;
+            connStat = locationAvailable;
+        } else {
+            connStat = locationNotAvailable;
+        }
+    }
 }
