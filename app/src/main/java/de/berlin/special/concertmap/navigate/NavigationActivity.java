c63f2f7bd8b53918311fdc89cd5ea723f1e06dda
Saeed Haddadi <saeid.special@gmail.com>
2015-11-30 14:36:39 +0100

    removing extra menu options code and xml files

--- app/src/main/java/de/berlin/special/concertmap/navigate/NavigationActivity.java
+++ app/src/main/java/de/berlin/special/concertmap/navigate/NavigationActivity.java
@@ -215,7 +215,7 @@ public class NavigationActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.navigation, menu);
         return true;
     }
 
